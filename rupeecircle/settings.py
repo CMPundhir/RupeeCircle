453c8d97a158895c0e648fae5cef72eab35485b2
CMPundhir <g.pundhir8802805015@gmail.com>
2023-06-27 16:09:20 +0530

    need to whitelist ip

--- rupeecircle/settings.py
+++ rupeecircle/settings.py
@@ -104,6 +104,7 @@ WSGI_APPLICATION = 'rupeecircle.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
+    # need to whitelist ip
     'default': {
 		'ENGINE': 'django.db.backends.postgresql',
 		'NAME': 'postgres',
