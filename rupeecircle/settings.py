335110e367a47a597e8c34a0bbbdbbd3b7201526
CMPundhir <g.pundhir8802805015@gmail.com>
2023-07-10 18:10:42 +0530

    dotenv

--- rupeecircle/settings.py
+++ rupeecircle/settings.py
@@ -17,6 +17,7 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
+# Redeploy
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
