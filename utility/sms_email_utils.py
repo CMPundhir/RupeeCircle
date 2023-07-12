03bbf28fa59ce4cecf8ac3a5340488b1d26a7b47
CMPundhir <g.pundhir8802805015@gmail.com>
2023-07-12 20:24:13 +0530

    email template update

--- utility/sms_email_utils.py
+++ utility/sms_email_utils.py
@@ -27,6 +27,23 @@ def sendEmailOtp(email):
     sender = 'support@rupeecircle.com'
 
     html_content = f"<strong>Your verification OTP is <h2>{otp}</h2></strong>"
+    html_content= f'''<div style="font-family: Helvetica,Arial,sans-serif;min-width:1000px;overflow:auto;line-height:2">
+  <div style="margin:50px auto;width:70%;padding:20px 0">
+    <div style="border-bottom:1px solid #eee">
+      <a href="" style="font-size:1.4em;color: #00466a;text-decoration:none;font-weight:600">RupeeCircle</a>
+    </div>
+    <p style="font-size:1.1em">Hi,</p>
+    <p>Thank you for choosing RupeeCircle. Use the following OTP to complete your Sign Up procedures. OTP is valid for 5 minutes</p>
+    <h2 style="background: #00466a;margin: 0 auto;width: max-content;padding: 0 10px;color: #fff;border-radius: 4px;">{otp}</h2>
+    <p style="font-size:0.9em;">Regards,<br />RupeeCircle</p>
+    <hr style="border:none;border-top:1px solid #eee" />
+    <div style="float:right;padding:8px 0;color:#aaa;font-size:0.8em;line-height:1;font-weight:300">
+      <p>RupeeCircle Inc</p>
+      <p>Delhi</p>
+      <p>India</p>
+    </div>
+  </div>
+</div>'''
     email = EmailMessage("Email verification OTP", html_content, sender, [email])
     email.content_subtype = "html"
     status = False
